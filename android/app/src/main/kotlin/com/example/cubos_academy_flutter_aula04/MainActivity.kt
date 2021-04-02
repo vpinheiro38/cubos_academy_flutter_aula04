1fe2679276e2566405cb027500c95f76ad5e5e43
Victor Pinheiro <vpinheiro.aguiar@gmail.com>
2021-04-01 21:19:16 -0300

    Start project

---
+++ android/app/src/main/kotlin/com/example/cubos_academy_flutter_aula04/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cubos_academy_flutter_aula04
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: CounterApp(),
+  ));
+}
+
+class CounterApp extends StatefulWidget {
+  @override
+  _CounterAppState createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
