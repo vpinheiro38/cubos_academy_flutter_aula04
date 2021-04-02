ec5871ef59ac965c84c5fafb0cbf97ef6c8ac76a
Victor Pinheiro <vpinheiro.aguiar@gmail.com>
2021-04-01 21:34:39 -0300

    Implement exercise

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,51 @@ class CounterApp extends StatefulWidget {
 }
 
 class _CounterAppState extends State<CounterApp> {
+  var _counter = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Aplicativo Contador')),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: Text(
+            "O valor atual do contador é:",
+            style: TextStyle(fontSize: 24),
+          )),
+          Center(
+              child: Text(
+            "$_counter",
+            style: TextStyle(fontSize: 34),
+          ))
+        ],
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                _counter += 1;
+              });
+            },
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                _counter -= 1;
+              });
+            },
+            child: Icon(Icons.remove),
+          )
+        ],
+      ),
+    );
   }
 }
